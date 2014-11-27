967cbcde2a88e52d2a5efd93b34970484656189b
steven.xu <steven.xu@CHNSHAD140023.internal.sungard.corp>
2014-11-27 11:27:41 +0800

    out of home

--- sevenup-rest/src/main/java/org/sevenup/rest/controller/user/UserController.java
+++ sevenup-rest/src/main/java/org/sevenup/rest/controller/user/UserController.java
@@ -2,17 +2,23 @@ package org.sevenup.rest.controller.user;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import org.sevenup.rest.domain.user.User;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @Produces(MediaType.APPLICATION_JSON)
@@ -21,7 +27,7 @@ public class UserController {
 	@RequestMapping(value ="/users",method = RequestMethod.GET)
 	@ResponseStatus(HttpStatus.OK)
 	@ResponseBody
-	public List<User> getAllAccounts() {
+	public List<User> getUsers() {
 		List<User> userList = new ArrayList<User>();
 		for (int i = 0; i < 10; i++) {
 			User tempUser = new User();
@@ -31,4 +37,32 @@ public class UserController {
 		return userList;
 	}
 
+	@RequestMapping(method = RequestMethod.GET, value = "user/{id}")
+	public ResponseEntity<User> viewUser(@PathVariable String id) {
+		
+		User user = new User();
+		user.setName("nonumber1989");
+		user.setLocactionName("ShangHai");
+		return new ResponseEntity<User>(user, HttpStatus.OK);
+     // return new ResponseEntity<User>(HttpStatus.NOT_FOUND);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<User> createUser(@RequestBody User User,UriComponentsBuilder builder) {
+		UUID idKey = UUID.randomUUID();
+		HttpHeaders headers = new HttpHeaders();
+		headers.setLocation(builder.path("/user/{id}").buildAndExpand(idKey.toString()).toUri());
+		return new ResponseEntity<User>(User, headers, HttpStatus.CREATED);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST, value = "signIn")
+	@ResponseBody
+	public boolean signIn(@RequestBody User User) {
+		return true;
+	}
+
+	@RequestMapping(method = RequestMethod.POST, value = "signUp")
+	public ResponseEntity<User> signUp(@RequestBody User User) {
+		return new ResponseEntity<User>(User, HttpStatus.OK);
+	}
 }
