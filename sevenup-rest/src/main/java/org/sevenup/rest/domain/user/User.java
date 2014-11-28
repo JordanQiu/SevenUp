ff2a4a66663eb346b8a6169245997448dd02a663
steven.xu <steven.xu@CHNSHAD140023.internal.sungard.corp>
2014-11-28 14:35:24 +0800

    add some domain

--- sevenup-rest/src/main/java/org/sevenup/rest/domain/user/User.java
+++ sevenup-rest/src/main/java/org/sevenup/rest/domain/user/User.java
@@ -3,9 +3,12 @@ package org.sevenup.rest.domain.user;
 import java.io.Serializable;
 import java.util.Date;
 
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.ResourceSupport;
-
+@XmlRootElement(name = "user")
 public class User extends ResourceSupport implements Serializable{
 
 	/**
@@ -26,54 +29,63 @@ public class User extends ResourceSupport implements Serializable{
 	public String getUid() {
 		return uid;
 	}
+	@XmlElement  
 	public void setUid(String uid) {
 		this.uid = uid;
 	}
 	public String getName() {
 		return name;
 	}
+	@XmlElement  
 	public void setName(String name) {
 		this.name = name;
 	}
 	public String getAvatar() {
 		return avatar;
 	}
+	@XmlElement  
 	public void setAvatar(String avatar) {
 		this.avatar = avatar;
 	}
 	public String getAlt() {
 		return alt;
 	}
+	@XmlElement  
 	public void setAlt(String alt) {
 		this.alt = alt;
 	}
 	public String getRelation() {
 		return relation;
 	}
+	@XmlElement  
 	public void setRelation(String relation) {
 		this.relation = relation;
 	}
 	public String getCreatedTime() {
 		return createdTime;
 	}
+	@XmlElement  
 	public void setCreatedTime(String createdTime) {
 		this.createdTime = createdTime;
 	}
 	public String getLocactionId() {
 		return locactionId;
 	}
+	@XmlElement  
 	public void setLocactionId(String locactionId) {
 		this.locactionId = locactionId;
 	}
 	public String getLocactionName() {
 		return locactionName;
 	}
+	@XmlElement  
 	public void setLocactionName(String locactionName) {
 		this.locactionName = locactionName;
 	}
 	public String getDescription() {
 		return description;
 	}
+	@XmlElement  
 	public void setDescription(String description) {
 		this.description = description;
 	}
